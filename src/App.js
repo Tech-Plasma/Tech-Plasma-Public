8ccee8331d0d82e97699b9f8a38c76a2fd7f833f
Srisank Shaurya <139223603+Black-Walkers-Army@users.noreply.github.com>
2023-10-18 14:12:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,35 +3,8 @@ import { useState, useEffect } from "react";
 export default function App () {
       const [text, setText] = useState('');
       return (
-            <div className="container" style={
-            {
-                  padding:0,
-                  margine:0,
-            }
-            }>
-                  <div style={ 
-                  { 
-                        background:"blue",
-                        color:"white",
-                        width:"100%",
-                        position:"fixed",
-                        top:"0%",
-                        left:"0%",
-                  }
-                  }>
-                        <center>
-                              <h1>Welcome to may React App</h1>
-                        </center>
-                  </div>
-                  <img src='./logo192.png' style={
-                  {
-                        width:"50%",
-                        height:"80%",
-                        position:"fixed",
-                        top:'10%',
-                        left:'25%',
-                  }
-                  }/>
-            </div>
+            <>
+                  <h1>Under constriction</h1>
+            </>
       );
 }
